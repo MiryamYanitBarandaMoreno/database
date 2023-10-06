507a1b3e0562516647523977d440965b7b11cb73
Miryam Yanit Baranda Moreno <miryamybm@gmail.com>
2023-10-06 21:03:44 +0200

    Configuracion a la conexion de la BD MySQL

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = {
+    host: 'localhost',
+    port: 3306,
+    database: 'backend',
+    user: 'root',
+    password: ''
+};
+
+const conn = mysql.createConnection(connection);
+
+conn.connect((err)=>{
+    if (err){
+        console.log("Error occurred while connecting to MySQL database.")
+    }else{
+        console.log("Connecting with MySQL database created successfully.")
+    }
+})
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,9 @@ const router = Router();
 
 //http://localhost:3000/api/v1/users/??
 router.get('/',listUsers);
+//router.post('/',listUsers);
+//router.put('/',listUsers);
+//router.patch('/',listUsers);
+//router.delete('/',listUsers);
 
 module.exports= router;
